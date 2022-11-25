545a1a8793b7a84c5a5d9ccd81ac35c85e13ec04
yukimemi <yukimemi@gmail.com>
2022-11-25 14:00:21 +0900

    :bug: Fix qa after socket close.

--- denops/hitori/main.ts
+++ denops/hitori/main.ts
@@ -47,13 +47,14 @@ export async function main(denops: Denops): Promise<void> {
         const ws = new WebSocket(`ws://localhost:${port}`);
         ws.onopen = async () => {
           clog(`[client] open socket !`);
-          if (quit) {
-            await denops.cmd(`qa!`);
-          }
+          await denops.cmd(`bwipeout!`);
           clog(`[client] send buf path: ${bufPath}`);
           ws.send(bufPath);
           clog(`[client] close socket !`);
           ws.close();
+          if (quit) {
+            await denops.cmd(`qa!`);
+          }
         };
       } catch (e) {
         console.log(e);
