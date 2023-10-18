4db4c9c789a0d0aa609977a487ade1795bfed57b
GitHub <noreply@github.com>
2023-10-18 06:06:07 +0000

    chore(deps): Update deno dependencies

--- denops/hitori/main.ts
+++ denops/hitori/main.ts
@@ -2,7 +2,7 @@ import * as fn from "https://deno.land/x/denops_std@v5.0.1/function/mod.ts";
 import * as helper from "https://deno.land/x/denops_std@v5.0.1/helper/mod.ts";
 import * as vars from "https://deno.land/x/denops_std@v5.0.1/variable/mod.ts";
 import * as buffer from "https://deno.land/x/denops_std@v5.0.1/buffer/mod.ts";
-import { ensure, is } from "https://deno.land/x/unknownutil@v3.9.0/mod.ts";
+import { ensure, is } from "https://deno.land/x/unknownutil@v3.10.0/mod.ts";
 import type { Denops } from "https://deno.land/x/denops_std@v5.0.1/mod.ts";
 
 let enable = true;
