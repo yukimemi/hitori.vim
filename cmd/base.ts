6dcd8df97477cd66316035e21539470cc6d4fa9d
GitHub <noreply@github.com>
2024-04-11 06:05:57 +0000

    chore(deps): Update deno dependencies

--- cmd/base.ts
+++ cmd/base.ts
@@ -1,4 +1,4 @@
-import { isAbsolute, join, normalize } from "https://deno.land/std@0.221.0/path/mod.ts";
+import { isAbsolute, join, normalize } from "https://deno.land/std@0.222.0/path/mod.ts";
 
 import { Command, EnumType } from "https://deno.land/x/cliffy@v1.0.0-rc.4/command/mod.ts";
 import { ensure, is, isArrayOf } from "https://deno.land/x/unknownutil@v3.17.2/mod.ts";
