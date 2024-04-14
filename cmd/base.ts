4f7a1a8ff66f6b460d16d318f60f989cf7513d4a
GitHub <noreply@github.com>
2024-04-14 07:31:14 +0000

    chore(deps): Update deno dependencies

--- cmd/base.ts
+++ cmd/base.ts
@@ -1,7 +1,7 @@
 import { isAbsolute, join, normalize } from "https://deno.land/std@0.222.1/path/mod.ts";
 
 import { Command, EnumType } from "https://deno.land/x/cliffy@v1.0.0-rc.4/command/mod.ts";
-import { ensure, is, isArrayOf } from "https://deno.land/x/unknownutil@v3.17.2/mod.ts";
+import { ensure, is, isArrayOf } from "https://deno.land/x/unknownutil@v3.17.3/mod.ts";
 
 const logLevelType = new EnumType(["debug", "info", "warn", "error"]);
 
